ab4feced9637c261fa3a40582e5b79b43c411a6e
MorganRohan <10355@coderacademy.edu.au>
2022-06-28 17:17:46 +1000

    implement account authentication

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,8 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: %i[ show edit update destroy ]
-  before_action :set_form_vars
+  before_action :set_form_vars, only: %i[ new edit ]
+  before_action :authenticate_account!, except: [:index, :show]
+
 
   # GET /listings or /listings.json
   def index
