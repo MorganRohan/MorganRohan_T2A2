857ae4570a16f7ea38daed687f32e3e06a824416
MorganRohan <10355@coderacademy.edu.au>
2022-07-05 13:36:54 +1000

    format image display size

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -7,7 +7,7 @@ module ListingsHelper
     end
 
     def format_sold(sold)
-        words = sold.to_s.map do |word|
+        words = sold.map do |word|
             word.capitalize
         end
     end
